a60e8621b21f39d486a7d2c2d51fe82a98ee8343
Navani <navaninavani786@gmail.com>
2020-12-03 14:44:28 +0530

    updating redux and Stripe api also adding it user authetication firebase for shop clothing.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HuDdNBXxOZuPwLaDU8Nb9aldV2N6UWPzNbzJxFnaHD7BqrS0CmZA0XRmGKMISfbkS4iF2JuWhg5rAC23SPVp4hp001X7BhwqY';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Doolo pvt.ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
